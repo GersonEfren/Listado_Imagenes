34c275836b78ea0ffaee78ec74572fce2282d3e3
FlutLab.io <>
2021-05-11 19:57:47 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(debugShowCheckedModeBanner: false, home: new PaginaInicio(title: "GALERIA"));
+  }
+}
+
+class PaginaInicio extends StatefulWidget {
+  final String title;
+  @override
+  PaginaInicio({this.title}) : super();
+  @override
+  State<StatefulWidget> createState() {
+    return new PaginaInicioState();
+  }
+}
+
+class PaginaInicioState extends State<PaginaInicio> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new GridView.extent(
+        maxCrossAxisExtent: 150.0,
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        padding: const EdgeInsets.all(5),
+        children: _buildGridTiles(29),
+      ),
+    );
+  }
+}
+
+List<Widget> _buildGridTiles(numberOfTiles) {
+  List<Container> containers = new List<Container>.generate(numberOfTiles, 
+  (int index) {
+    //index = 0, 1, 2,...
+    final imageName = index < 9 ? 'images/image0${index + 1}.JPG' : 'images/image${index + 1}.JPG';
+    return new Container(
+      child: new Image.asset(imageName, fit: BoxFit.fill),
+    );
+  });
+  return containers;
+}
